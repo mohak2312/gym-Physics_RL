9f2de6101416b338e6898817a155f58bac73e95e
Mohak <mohak.patel2312@gmail.com>
2019-06-01 22:23:44 -0700

    update
    dependencies and  timestamp

--- gym_Physics_RL/__init__.py
+++ gym_Physics_RL/__init__.py
@@ -3,8 +3,10 @@ from gym.envs.registration import register
 register(
     id='Physics_RL-v0',
     entry_point='gym_Physics_RL.envs:Physics_RLEnv',
+    timestep_limit=2000,
 )
 register(
     id='Physics_RL-extrahard-v0',
     entry_point='gym_Physics_RL.envs:Physics_RLExtraHardEnv',
+    timestep_limit=2000,
 )
